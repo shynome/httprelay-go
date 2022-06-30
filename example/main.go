e07f721909d9c071781fb05e2f17b3e8ab9944f2
shynome <shynome@gmail.com>
2022-07-01 04:08:40 +0800

    proxy var name is better

--- example/main.go
+++ example/main.go
@@ -8,14 +8,15 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "hello world")
 	})
-	server := "http://127.0.0.1:8080"
-	auth := httprelay.ProxyAuth{ID: "aaaaa", Secret: "88888888"}
-	relay := httprelay.NewProxy(server)
-	// relay.Parallel = 1
-	relay.Auth = auth
-	fmt.Printf("%shello \n", relay.GetServerUrl())
-	relay.Serve(nil)
+
+	proxy := httprelay.NewProxy("http://127.0.0.1:8080")
+	proxy.Auth = httprelay.ProxyAuth{ID: "aaaaa", Secret: "88888888"}
+	proxy.Parallel = 1
+
+	fmt.Printf("%shello \n", proxy.GetServerUrl())
+	proxy.Serve(nil)
 }
